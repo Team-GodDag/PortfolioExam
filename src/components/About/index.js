d667b96e62812ab54228eb502d0bd69474233c5a
Champen <dawgen@gmail.com>
2022-05-23 18:03:35 +0200

    mandags Jamm post Henrik

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,7 +16,7 @@ const About = () => {
   return (
     <div className="container about-page">
       <div className="wave" style={{ background: " #66BBC6" }}></div>
-      <div className="text-zone about-text">
+      <div className="text-zone ">
         <h1>
           <AnimatedLetters
             letterClass={letterClass}
